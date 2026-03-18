bf79c111aeb9f1e89a90d06e4fe550bedc25f13e
misho <misho.niki2003@gmail.com>
2026-03-18 16:57:02 +0200

    added screenshots

--- MyCook/settings.py
+++ MyCook/settings.py
@@ -117,8 +117,11 @@ DATABASES = {
 # }
 
 
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Password validation
--- Recipes/admin.py
+++ Recipes/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from Recipes.models import Profile
+from Recipes.models import Profile, Recipe, RecipeIngredient
 
 admin.site.register(Profile)
+admin.site.register(Recipe)
+admin.site.register(RecipeIngredient)
+
